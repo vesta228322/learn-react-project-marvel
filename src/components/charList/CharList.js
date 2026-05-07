32e9755f880ab5ee1249ecfa8fcc0fb5a18a3f2e
Vesta <hotabych39@gmail.com>
2026-05-07 21:39:02 +0300

    implement CharList component and add prop-types dependency

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,7 +12,8 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 12
+        offset: 12,
+        charEnd: false
     }
 
     marvelService = new MarvelService();
@@ -35,11 +36,16 @@ class CharList extends Component {
     }
 
     onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
         this.setState(({ offset, charList }) => ({
             charList: [...charList, ...newCharList],
             loading: false,
             newItemLoading: false,
-            offset: offset + 9
+            offset: offset + 9,
+            charEnd: ended
         }));
     }
 
@@ -72,12 +78,14 @@ class CharList extends Component {
 
 
     render() {
-        const { charList, loading, error, newItemLoading, offset } = this.state;
+        const { charList, loading, error, newItemLoading, offset, charEnd } = this.state;
 
         const items = this.renderItems(charList);
         const errorMessage = error ? <ErrorMessage /> : null,
             spinner = loading ? <Spinner /> : null,
             content = !(error || loading) ? items : null;
+
+
         return (
             <div className="char__list">
                 {errorMessage}
@@ -86,7 +94,8 @@ class CharList extends Component {
                 <button
                     className="button button__main button__long"
                     disabled={newItemLoading}
-                    onClick={() => this.onRequest(offset)} >
+                    onClick={() => this.onRequest(offset)}
+                    style={{ 'display': charEnd ? 'none' : 'block' }} >
                     <div className="inner">load more</div>
                 </button>
             </div>
